cd3a71e9de2cc8653b4e9865094729baaa100198
Sagar Bhandari <rsaagar7200@gmail.com>
2026-02-25 14:07:15 +0545

    js variables

---
+++ 1.variables.js
@@ -0,0 +1,122 @@
+// synchronous , interpreted, dynamic typed language
+
+console.log('hello world');
+
+console.log(2 + 2);
+
+//variables
+// let var const
+var a = 30
+// var a = 30
+
+a = 'str'
+console.log(a)
+
+// var a = ''
+
+
+
+
+
+let b ='hello'
+// let b;
+
+// let b
+b = 87
+console.log(b)
+
+b ='jhd'
+console.log(b)
+
+const c =56
+// c = 67
+console.log(c)
+
+const d =56
+let e;
+
+// let x;
+e =56
+
+// const c = ''
+
+// var b = ''
+
+//! data types
+//! primitive
+// string , number , boolean , undefiend , null
+
+//? string
+let str = 'hello';
+let f = "world"
+//! template literal
+console.log(f)
+let g = `hello ${2 + 2}`
+// console.log(g)
+
+//? number
+let h = 12
+let i = 12.45
+
+//? boolean
+let j = true
+let k = false
+
+//? undefined
+// let l = undefined;
+let l;
+console.log(l)
+
+//? null
+let m = null
+
+console.log(m)
+
+
+//! operators
+
+
+
+//! non primitive
+//* object 
+//* array
+//* function
+
+
+
+
+
+// vaiables
+// let const var
+let x;
+x = 74
+// let x = 56
+
+var y ;
+ y = 57
+
+ var y = 465
+
+ const z = 56;
+//  z = 8
+// const z = 56
+
+//! data types;
+// number 
+// string
+// boolean
+// null  => empty
+// let a = null
+// undefined  => 
+    // let variable;
+
+//! operators
+
+// l
+
+
+
+
+
+
+
