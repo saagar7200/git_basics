4886923d2c95661c723bfe763b77f236fbb5888c
Sagar Bhandari <rsaagar7200@gmail.com>
2026-04-24 17:47:51 +0545

    git commands

---
+++ git.js
@@ -0,0 +1,14 @@
+//! git & github/gitlab/bitbucket
+//? git => vcs
+
+//* commands
+//? git config --global --list  => list local git global config
+//? git config --global user.email "abc@gmail.com"
+
+//? git init   => initialize empty git repo. at folder
+
+//! WD  => Staging Area => local repo commit
+
+//? git status
+//? git add <file_path>  => wd -> SA
+//? git commit -m "<commit message>"  => SA  -> local branch/repo
